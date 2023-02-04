212f1e680aff708633ee54883441e1986e0652ca
rgmvp <perez4horoneniepwnz@yandex.ru>
2023-02-04 05:27:16 +0300

    dne

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -2,9 +2,9 @@ package com.epam.mjc.stage0;
 
 public class Animal {
 
-    String color;
-    int numberOfPaws;
-    boolean hasFur;
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
     Animal (String color, int numberOfPaws, boolean hasFur){
         this.color = color;
@@ -15,24 +15,13 @@ public class Animal {
     public String getDescription(){
 
 
-        if (hasFur && numberOfPaws > 1){
-        return "This animal is mostly "+color+". It has "+ ""+ numberOfPaws+" paws and "+"a"+" fur.";
+        if (hasFur == true){
+        return "This animal is mostly "+color+". It has "+ numberOfPaws+" paws and a fur.";
         }
-        else if (hasFur && numberOfPaws < 2)
-            return "This animal is mostly "+color+". It has "+ "" +numberOfPaws+" paw and "+"a"+" fur.";
-        else if (hasFur && numberOfPaws > 1)
-            return "This animal is mostly "+color+". It has "+ "" +numberOfPaws+" paws and "+"no"+" fur.";
-        else if(!hasFur && numberOfPaws < 2)
-            return "This animal is mostly "+color+". It has "+"" +numberOfPaws+" paw and "+"no"+" fur.";
-        else
-            return "";
+        return "This animal is mostly "+color+". It has "+numberOfPaws+" paws and no fur.";
+
     }
 
-    public static void main(String[] args) {
-        Bird brd = new Bird();
-        Dog dg = new Dog();
-        dg.getDescription();
-        brd.getDescription();
-    }
+
 
 }
